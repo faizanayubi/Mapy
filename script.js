663ec238718f277dcb082edff802b7e991cb4eb5
faizanayubi <indianayubi@gmail.com>
2015-04-02 20:05:25 +0530

    Add Marker Prototype added

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-(function(window, google, mapster) {
-	
-	//map options
-	var options = mapster.MAP_OPTIONS,
-	element = document.getElementById('map-canvas');
-	//map
-	map = new google.maps.Map(element, options);
+(function (window, mapster) {
 
-}(window, google, window.Mapster || (window.Mapster = {})));
+    //map options
+    var options = mapster.MAP_OPTIONS,
+            element = document.getElementById('map-canvas');
+    //map
+    map = mapster.create(element, options);
+
+    map.addMarker({
+        lat : 37.791350,
+        lng : 122.435883,
+        visible : true,
+        draggable : true,
+        id : 1
+    });
+
+}(window, window.Mapster || (window.Mapster = {})));
